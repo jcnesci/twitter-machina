893b392d99f4cb9ff83d419ddb0032ea96605a2f
Colin Wageman <colin@sosolimited.com>
2014-06-17 16:43:50 -0700

    added word property of visibility based on duplicate words

--- public/javascripts/tweet.js
+++ public/javascripts/tweet.js
@@ -1,6 +1,6 @@
 var words = [];	// An array for the collection of all the words.
 var allTweets = [];	// An array to hold all the tweets.
-
+var lookup = {};
 // Creates the individual tweet objects.
 function Tweet(tweet, refSet) {
 	this.fullTweet = tweet;
@@ -10,9 +10,13 @@ function Tweet(tweet, refSet) {
 	allTweets.push(tweet);
 	
 	for (var i = 0; i < tmpWords.length; i++) {
-		// $.inArray(value, array)
-
-		words.push(new Word(tmpWords[i], refSet));
+    	if (lookup[tmpWords[i]] == undefined) {
+    		lookup[tmpWords[i]] = 1;
+    		words.push(new Word(tmpWords[i], refSet, true));
+    	} else {
+    		lookup[tmpWords[i]] = 1 + lookup[tmpWords[i]];
+    		words.push(new Word(tmpWords[i], refSet, false));
+    	}		
 	}
 
 };
--- public/javascripts/word.js
+++ public/javascripts/word.js
@@ -1,6 +1,8 @@
 // Creates the individual word objects.
-function Word(theWord, refSet) {
+function Word(theWord, refSet, vis) {
 	this.value = theWord;
 	this.linkedSets = [refSet];
-	this.linkedTweets = [{}];	
+	this.linkedTweets = [{}];
+	//this.count = 1;
+	this.visible = vis;	
 };
