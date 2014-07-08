e01c79df58887793e52b04017cf49bf353172807
Colin Wageman <colin@sosolimited.com>
2014-07-07 17:38:50 -0700

    added typeahead js to search(needs to be linked to twitter), Tweet request pull upto 5 times to fill desired amount of tweets.

--- public/javascripts/view.js
+++ public/javascripts/view.js
@@ -309,9 +309,55 @@ function updateView(){
 
 function searchView() {
 	$('#searchContainer').append('<div id="searchForm"><div>'+
-		'Person 1: <input id="input1" type="text" name="person1" value="DalaiLama">'+
-		'Person 2: <input id="input2" type="text" name="person2" value="PutinRF_Eng">'+
+		'Person 1: <input id="input1" class="typeahead" type="text" name="person1" value="DalaiLama">'+
+		'Person 2: <input id="input2" class="typeahead" type="text" name="person2" value="PutinRF_Eng">'+
 		'<input type="submit" value="Submit" onclick="searchButton(); return false;"></div></div>');
+
+	var substringMatcher = function(strs) {
+	  return function findMatches(q, cb) {
+	    var matches, substringRegex;
+	 
+	    // an array that will be populated with substring matches
+	    matches = [];
+	 
+	    // regex used to determine if a string contains the substring `q`
+	    substrRegex = new RegExp(q, 'i');
+	 
+	    // iterate through the pool of strings and for any string that
+	    // contains the substring `q`, add it to the `matches` array
+	    $.each(strs, function(i, str) {
+	      if (substrRegex.test(str)) {
+	        // the typeahead jQuery plugin expects suggestions to a
+	        // JavaScript object, refer to typeahead docs for more info
+	        matches.push({ value: str });
+	      }
+	    });
+	 
+	    cb(matches);
+	  };
+	};
+	 
+	var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+	  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+	  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+	  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+	  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+	  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+	  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+	  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+	  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+	];
+	 
+	$('#searchForm .typeahead').typeahead({
+	  hint: true,
+	  highlight: true,
+	  minLength: 1
+	},
+	{
+	  name: 'states',
+	  displayKey: 'value',
+	  source: substringMatcher(states)
+	});
 }
 
 function searchButton() {
